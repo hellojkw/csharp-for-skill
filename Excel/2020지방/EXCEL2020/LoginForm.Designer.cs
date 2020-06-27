f95a972e8f012bacfcb08d8e5149f84a66b9720f
jkw <jkwchunjae@daum.net>
2020-06-28 01:50:24 +0900

    로그인폼 UI

--- Excel/2020지방/EXCEL2020/LoginForm.Designer.cs
+++ Excel/2020지방/EXCEL2020/LoginForm.Designer.cs
@@ -28,21 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.IdText = new System.Windows.Forms.TextBox();
+            this.PasswordText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.RegisterButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // IdText
+            // 
+            this.IdText.Location = new System.Drawing.Point(77, 26);
+            this.IdText.Name = "IdText";
+            this.IdText.Size = new System.Drawing.Size(229, 27);
+            this.IdText.TabIndex = 1;
+            // 
+            // PasswordText
+            // 
+            this.PasswordText.Location = new System.Drawing.Point(77, 74);
+            this.PasswordText.Name = "PasswordText";
+            this.PasswordText.PasswordChar = '●';
+            this.PasswordText.Size = new System.Drawing.Size(229, 27);
+            this.PasswordText.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PW";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(25, 120);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(115, 38);
+            this.LoginButton.TabIndex = 4;
+            this.LoginButton.Text = "로그인";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(191, 120);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(115, 38);
+            this.RegisterButton.TabIndex = 5;
+            this.RegisterButton.Text = "회원가입";
+            this.RegisterButton.UseVisualStyleBackColor = true;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 294);
+            this.ClientSize = new System.Drawing.Size(341, 184);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PasswordText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.IdText);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Malgun Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "LoginForm";
             this.Text = "로그인";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IdText;
+        private System.Windows.Forms.TextBox PasswordText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button RegisterButton;
     }
 }
