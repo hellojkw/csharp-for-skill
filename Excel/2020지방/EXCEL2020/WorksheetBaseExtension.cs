0768e11c5561175925a1c2fd7e7bce2ef81b76bd
jkw <jkwchunjae@daum.net>
2020-06-28 01:46:20 +0900

    시트 버튼 추가를 위한 extension

---
+++ Excel/2020지방/EXCEL2020/WorksheetBaseExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Tools = Microsoft.Office.Tools.Excel;
+
+namespace EXCEL2020
+{
+    public static class WorksheetBaseExtension
+    {
+        public static Tools.Controls.Button CreateButton(this Tools.WorksheetBase worksheetBase, string buttonName, Excel.Range range, Action action)
+        {
+            var button = new Tools.Controls.Button();
+            button.Text = buttonName;
+            button.Click += (sender, args) => action?.Invoke();
+            worksheetBase.Controls.AddControl(button, range, buttonName);
+
+            return button;
+        }
+    }
+}
