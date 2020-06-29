2cfa0e8504678d260b6b3db42815a3ed322fd4c6
jkw <jkwchunjae@daum.net>
2020-06-30 00:10:02 +0900

    IEnumerable.Empty 추가

---
+++ Excel/2020지방/EXCEL2020/EnumerableExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXCEL2020
+{
+    public static class EnumerableExtension
+    {
+        public static bool Empty<T>(this IEnumerable<T> source)
+        {
+            return !source.Any();
+        }
+
+        public static bool Empty<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            return !source.Any(predicate);
+        }
+    }
+}
